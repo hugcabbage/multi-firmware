c50f5ed170fb6eae085f59a35a1a3dc100c67338
hugcabbage <77980779+hugcabbage@users.noreply.github.com>
2022-06-03 22:48:44 +0800

    fix path

--- extra-files/unblockmusic.sh
+++ extra-files/unblockmusic.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i '/UnblockNeteaseMusic_NodeJS/d' .config
-sed -i -e '/unblockmusic/ s/# //g' -e '/unblockmusic/ s/ is not set/=y/g' .config
+sed -i '/UnblockNeteaseMusic_NodeJS/d' preset-models/temp.config
+sed -i -e '/unblockmusic/ s/# //g' -e '/unblockmusic/ s/ is not set/=y/g' preset-models/temp.config
